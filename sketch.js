703f458ce5a03cba75a02b4eca4e1cc82a131953
BLOPguy64 <72127787+BLOPguy64@users.noreply.github.com>
2020-10-11 18:15:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var wall,thickness;
+var bullet,speed,weight 
+
+function setup() {
+  createCanvas(800,400);
+thickness=random(22,83)
+speed=random(223,321)
+weight=random(30,52)
+wall=createSprite(1200,200,thickness,height/2);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+
+function hasCollided(bullet,wall)
+{
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+{
+   return true
+}
+  return false
+}
+
+if(hasCollided(bullet,wall))
+{
+bullet.velocityX=0;
+var damage=0.5*weight*speed*speed/(thickness*thicknees*thickness)
+
+
+if(damage>10)
+{
+  wall.shapeColor=color(255,0,0)
+
+}
+
+if(damage<10)
+{
+  wall.shapeColor=color(0,255,0)
+}
+}
+
+
+
+
+
